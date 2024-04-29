2cb2aafc8fe054380e7d4b6c0e0844b1dced912b
Vivek-Rajani <168379230+Vivek-Rajani@users.noreply.github.com>
2024-04-28 20:50:39 -0400

    Deleted directory for reuploading

--- customer_support_project/form/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import form, submit_form, submission
-
-urlpatterns = [
-    path('', form, name='form'),
-    path('submit/', submit_form, name='submit_form'),
-    path('submission/', submission, name='submission'),
-]
